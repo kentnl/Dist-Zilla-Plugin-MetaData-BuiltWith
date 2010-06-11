f611168acedffa8db61283a28ce9005ab1bec249
Kent Fredric <kentfredric@gmail.com>
2010-06-11 23:39:49 +1200

    Everything works, tests pass, readying for the shipping bro!

--- lib/Dist/Zilla/Plugin/MetaData/BuiltWith.pm
+++ lib/Dist/Zilla/Plugin/MetaData/BuiltWith.pm
@@ -4,32 +4,67 @@ package Dist::Zilla::Plugin::MetaData::BuiltWith;
 
 # ABSTRACT: Report what versions of things your distribution was built against
 
+
+=head1 SYNOPSIS
+
+  [MetaData::BuiltWith]
+  include = Some::Module::Thats::Not::In::Preq
+  exclude = Some::Module::Youre::Ashamed::Of
+
 =head1 DESCRIPTION
 
-Often, distribution authors get module dependencies wrong. So in such cases, 
+Often, distribution authors get module dependencies wrong. So in such cases,
 its handy to be able to see what version of various packages they built with.
 
-Some would prefer to demand everyone install the same version as they did, 
+Some would prefer to demand everyone install the same version as they did,
 but thats also not always nessecary.
 
-Hopefully, the existance of the metadata provided by this module will help 
-users on thier end machines make intelligent choices about what modules to 
+Hopefully, the existance of the metadata provided by this module will help
+users on thier end machines make intelligent choices about what modules to
 install in the event of a problem.
+
+
+
+=head1 EXAMPLE OUTPUT ( META.json )
+
+    "x_BuiltWith" : {
+       "modules" : {
+          "Dist::Zilla::Role::MetaProvider" : "4.101612",
+          "File::Find" : "1.15",
+          "File::Temp" : "0.22",
+          "Module::Build" : "0.3607",
+          "Moose" : "1.07",
+          "Test::More" : "0.94"
+       },
+       "perl" : "5.012000",
+       "platform" : "MSWin32"
+    },
 
 =cut
 
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider';
-use Carp qw( carp );
+
+=method mvp_multivalue_args
+
+This module can take, as parameters, any volume of 'exclude' or 'include' arguments.
+
+=cut
+
+sub mvp_multivalue_args { qw( exclude include ) };
+
+has exclude => ( is => 'ro', isa => 'ArrayRef', default => sub { [] } );
+has include => ( is => 'ro', isa => 'ArrayRef', default => sub { [] } );
+
 
 sub _get_prereq_modnames {
   my ( $self ) = @_;
   my %modnames;
   my $prereqs = $self->zilla->prereqs->as_string_hash;
   return [] unless defined $prereqs;
-  for my $phase ( keys %{ $prereqs } ){ 
+  for my $phase ( keys %{ $prereqs } ){
     next unless defined $prereqs->{$phase};
-    for my $type ( keys %{ $prereqs->{$phase} } ){ 
+    for my $type ( keys %{ $prereqs->{$phase} } ){
       next unless defined $prereqs->{$phase}->{$type};
       for my $module ( keys %{ $prereqs->{$phase}->{$type} } ) {
           $modnames{$module} = 1;
@@ -39,58 +74,74 @@ sub _get_prereq_modnames {
   return [ sort { $a cmp $b } keys %modnames ];
 }
 
-{ my $context = 0; 
+{ my $context = 0;
 
   sub _logonce {
     my ( $self, $module, $reason, $error ) = @_ ;
     my $message = "Possible Error: Module '$module' $reason.";
-    if ( not $context and not $ENV{BUILTWITH_TRACE} ){ 
+    if ( not $context and not $ENV{BUILTWITH_TRACE} ){
       $context++;
       $message .= "set BUILTWITH_TRACE=1 for details";
     }
     $self->zilla->log($message);
     if( $ENV{BUILTWITH_TRACE} ){
-      $self->zilla->log('$@ : ' . $error->[0] ); 
-      $self->zilla->log('$! : ' . $error->[1] ); 
+      $self->zilla->log('$@ : ' . $error->[0] );
+      $self->zilla->log('$! : ' . $error->[1] );
     }
     return;
   }
 
 }
 
-sub _detect_installed { 
+sub _detect_installed {
   my ( $self, $module ) = @_;
   my $success = undef;
   eval "require $module; \$success = 1";
   my $lasterror = [ $@, $! ];
   if  ( not $success ){
-    $self->_logonce( $module, 'did not load' , $error );
-    return "NA(possibly not installed)"; 
+    $self->_logonce( $module, 'did not load' , $lasterror );
+    return "NA(possibly not installed)";
   }
   my $modver;
   $success = undef;
   eval "\$modver = $module->VERSION(); \$success = 1";
   $lasterror = [$@, $!];
-  if ( not $success ){ 
+  if ( not $success ){
     $self->_logonce( $module, ' died assessing its version', $lasterror );
     return "NA(version could not be resolved)";
   }
-  if ( not defined $modver ){ 
+  if ( not defined $modver ){
     $self->_logonce( $module, ' reported an undefined version', $lasterror );
     return "NA(undef)";
   }
   return "$modver";
 }
 
+=method metadata
+
+This module scrapes together the name of all modules that exist in the "Prereq" section
+that Dist::Zilla collects, and then works out what version of things you have,
+applies the various include/exclude rules, and ships that data back to Dist::Zilla
+via this method. See L<Dist::Zilla::Role::MetaProvider> for more details.
+
+=cut
+
 sub metadata {
   my ( $self )  = @_;
 
   my $report = $self->_get_prereq_modnames();
-  my %modtable = map { 
+  my %modtable = map {
     $_ , $self->_detect_installed($_)
-  } @$report;
+  } ( @{$report}, @{ $self->include } );
+  for  my $badmodule ( @{ $self->exclude } ){
+    delete $modtable{$badmodule} if exists $modtable{$badmodule};
+  }
 
-  return { x_BuiltWith => \%modtable };  
+  return { x_BuiltWith => {
+      modules   => \%modtable,
+      perl      => $],
+      platform  => $^O,
+  }};
 }
 
 1;
