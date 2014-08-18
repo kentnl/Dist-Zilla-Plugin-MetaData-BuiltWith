5a52fef38438a0f6d18def3ccd138c306870e4a3
Kent Fredric <kentfredric@gmail.com>
2014-08-18 15:27:25 +1200

    Build results of 312cd65 (on master)

--- lib/Dist/Zilla/Plugin/MetaData/BuiltWith.pm
+++ lib/Dist/Zilla/Plugin/MetaData/BuiltWith.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::MetaData::BuiltWith;
 
-our $VERSION = '1.002000';
+our $VERSION = '1.002001';
 
 # ABSTRACT: Report what versions of things your distribution was built against
 
@@ -392,7 +392,7 @@ Dist::Zilla::Plugin::MetaData::BuiltWith - Report what versions of things your d
 
 =head1 VERSION
 
-version 1.002000
+version 1.002001
 
 =head1 SYNOPSIS
 
